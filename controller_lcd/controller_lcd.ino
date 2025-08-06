fab26302f34811085e39a8f64f5c1d468bb08171
bo7midIlahi <elbo7ilahi@gmail.com>
2025-08-06 16:38:04 +0100

    ditched Communiactio.h for wire.h

--- controller_lcd/controller_lcd.ino
+++ controller_lcd/controller_lcd.ino
@@ -1,56 +1,29 @@
-#include "Communication.h"
-
-//Board adresses for the communication
-#define COLTROLLER_CMD_ADRESS 10
-#define COLTROLLER_LCD_ADRESS 25
-
-static Communication comms(COLTROLLER_LCD_ADRESS);
-
+#include <Wire.h>
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-/*#include <nRF24L01.h>
-#include <RF24.h>*/
-
-/*
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-*/
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
-
-U8G2_ST7920_128X64_F_8080 u8g2(U8G2_R0, 0, 1, 2, 3, 4, 5, 6, 7, /*enable=*/ 8 /* A4 */, /*cs=*/ U8X8_PIN_NONE, /*dc/rs=*/ 9 /* A3 */, /*reset=*/ /*10*/ A1);  // Remember to set R/W to 0 
-
-//defining radio
-/*
-RF24 radio(A2, A3); // CE, CSN
-const byte address[6] = "00001";
-
-struct joystick {
-  int x;
-  int y;
-  int btn;
-};
-
-struct data {
-  struct joystick joystick;
-  int throttle;
-  int rotation;
-  int light;
-};
-*/
+U8G2_ST7920_128X64_F_8080 u8g2(U8G2_R0, 10, 11, 2, 3, 4, 5, 6, 7, /*enable=*/ 8 /* A4 */, /*cs=*/ U8X8_PIN_NONE, /*dc/rs=*/ 9 /* A3 */, /*reset=*/ /*10*/ A1);  // Remember to set R/W to 0 
+const byte SLAVE_ADDRESS = 8;
+char receivedData[32]; // Adjust size as needed
 
 void setup(void) {
-  //Serial.begin(115200);
-  
-  /*radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();*/
-
+  Wire.begin(SLAVE_ADDRESS); // Join the I2C bus as slave with its address
+  Wire.onReceive(receiveEvent); // Register the receive event handler
+  Serial.begin(115200);
   u8g2.begin();
+}
+
+void receiveEvent(int howMany) {
+  int i = 0;
+  while (Wire.available()) { // Loop while there are bytes available
+    char c = Wire.read(); // Read a byte
+    if (i < sizeof(receivedData) - 1) { // Prevent buffer overflow
+      receivedData[i++] = c;
+    }
+  }
+  receivedData[i] = '\0'; // Null-terminate the received string
+  Serial.print("Received: ");
+  Serial.println(receivedData);
 }
 
 void drawSmileyFace() {
@@ -90,57 +63,7 @@ void drawBattery() {
   }
 }
 
-/*char* getSubString(int param){
-  char message = comms.GetStringMessage();
-  static char token[10];
-
-  memset(token, 0, sizeof(token));
-
-  switch (param) {
-    case 1:
-      for (int i=0; i<4; i++) {
-        token[i] = message[i];
-      }
-      break;
-    
-    case 2:
-      for (int i=0; i<4; i++) {
-        token[i] = message[i+5];
-      }
-      break;
-  }
-
-  return token;
-}*/
-
 void loop(void) {
-
-/*
-  struct data data;
-  if (radio.available()) {
-    radio.read(&data, sizeof(struct data));
-  }
-
-  /*preparng data to show on the lcd
-  const char joystickX[10];
-  sprintf(joystickX, "%d", data.joystick.x);
-  
-  const char joystickY[10];
-  sprintf(joystickY, "%d", data.joystick.y);
-
-  const char joystickBTN[10];
-  sprintf(joystickBTN, "%d", data.joystick.btn);
-
-  const char light[10];
-  sprintf(light, "%d", data.light);
-
-  const char throttle[10];
-  sprintf(throttle, "%d", data.throttle);
-
-
-  Serial.println("JOY X: ");
-  Serial.println(data.joystick.x);
-*/
   u8g2.clearBuffer();         // clear the internal memory
   u8g2.setFont(u8g2_font_04b_03_tr); // choose a suitable font
 
@@ -152,18 +75,10 @@ void loop(void) {
   u8g2.drawStr(20, 20, "118.2437W");
   
   u8g2.drawStr(0,30,"ALT:");  // write something to the internal memory
-
-  char message_received = comms.GetStringMessage();
-  /*String msgString = String(message_received);
-  String alt = msgString.substring(0,3);
-  char alttt[4];
-  alt.toCharArray(alttt, 4);*/
-  u8g2.drawStr(20, 30, message_received);
-  
+  u8g2.drawStr(20, 30, receivedData);
   
   //FLIGHT DATA
   u8g2.drawStr(78,10,"THR:");  // write something to the internal memory
-  //token = strtok(NULL,",");
   u8g2.drawStr(98, 10, "100%");
 
   u8g2.drawStr(78,20,"SAT:");  // write something to the internal memory
