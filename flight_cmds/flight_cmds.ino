e2a305dc838bff1881f72fc9a8fb877e0b1c18be
bo7midIlahi <elbo7ilahi@gmail.com>
2025-08-27 11:49:16 +0100

    fixed why the drone suddenly went full throttle
    it was because the throttle in userIn is char array, and i was converting it to int and mapping it, likely making it a huge value that's why the map function interpreted it as max speed for motors.
    
    now all why i did was converting the string to int using atoi and constraint it to fixed range.

--- flight_cmds/flight_cmds.ino
+++ flight_cmds/flight_cmds.ino
@@ -1,176 +1,3 @@
-/*#include <Servo.h>
-
-#include <SPI.h>
-#include <RF24.h>
-#include <nRF24L01.h>
-
-//nRF24L01
-RF24 radio(8, 7);
-const byte address[6] = "00010";
-
-Servo ESC1;
-Servo ESC2;
-Servo ESC3;
-Servo ESC4;
-
-#define MOTOR1LED 15
-#define MOTOR2LED 16
-#define MOTOR3LED 17
-#define MOTOR4LED 18
-
-struct userInputs {
-  char altitude[4];
-  char throttle[4];
-  char light[3];
-  char throttle_change_disable[3];
-  char hand_setup[3];
-  int emergency_landing;
-  int engine_cut;
-  int yaw;
-  int pitch;
-  int roll;
-};
-struct userInputs userIn;
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(2,OUTPUT);
-
-  ESC1.attach(3,1000,2000);
-  ESC2.attach(5,1000,2000);
-  ESC3.attach(6,1000,2000);
-  ESC4.attach(9,1000,2000);
-
-  //calibration
-  ESC1.writeMicroseconds(2000);
-  ESC2.writeMicroseconds(2000);
-  ESC3.writeMicroseconds(2000);
-  ESC4.writeMicroseconds(2000);
-
-  delay(3000);
-
-  ESC1.writeMicroseconds(1000);
-  ESC2.writeMicroseconds(1000);
-  ESC3.writeMicroseconds(1000);
-  ESC4.writeMicroseconds(1000);
-
-  delay(3000);
-
-  Serial.println("DONE CALIBRATION");
-
-  radio.begin();
-  radio.openReadingPipe(0,address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
-
-  //setup pins
-  pinMode(MOTOR1LED,OUTPUT);
-  pinMode(MOTOR2LED,OUTPUT);
-  pinMode(MOTOR3LED,OUTPUT);
-  pinMode(MOTOR4LED,OUTPUT);
-}
-
-void motorTest(){
-  for (int i=0; i<=1000; i+=50) {
-    ESC1.writeMicroseconds(1000 + i);
-    ESC2.writeMicroseconds(1000 + i);
-    ESC3.writeMicroseconds(1000 + i);
-    ESC4.writeMicroseconds(1000 + i);
-
-    Serial.print("INCREASING: ");
-    Serial.println(1000+i);
-  
-    delay(500);
-  }
-
-  delay(2000);
-
-  for (int i=1000; i>=0; i-=50) {
-    ESC1.writeMicroseconds(1000 + i);
-    ESC2.writeMicroseconds(1000 + i);
-    ESC3.writeMicroseconds(1000 + i);
-    ESC4.writeMicroseconds(1000 + i);
-
-    Serial.print("DECREASING: ");
-    Serial.println(1000+i);
-
-    delay(500);
-  }
-}
-
-void loop() {
-  if (radio.available()){
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-
-    digitalWrite(2,HIGH);
-
-    digitalWrite(MOTOR1LED,HIGH);
-    digitalWrite(MOTOR2LED,HIGH);
-    digitalWrite(MOTOR3LED,HIGH);
-    digitalWrite(MOTOR4LED,HIGH);
-
-    Serial.println(userIn.altitude);
-    Serial.println(userIn.emergency_landing);
-    Serial.println(userIn.engine_cut);
-    Serial.println(userIn.hand_setup);
-    Serial.println(userIn.pitch);
-    Serial.println(userIn.roll);
-    Serial.println(userIn.yaw);
-    Serial.println(userIn.throttle);
-  }else {
-    Serial.println("NOPE");
-    digitalWrite(2,HIGH);
-    digitalWrite(MOTOR1LED,HIGH);
-    digitalWrite(MOTOR2LED,HIGH);
-    digitalWrite(MOTOR3LED,HIGH);
-    digitalWrite(MOTOR4LED,HIGH);
-    delay(200);
-    digitalWrite(2,LOW);
-    digitalWrite(MOTOR1LED,LOW);
-    digitalWrite(MOTOR2LED,LOW);
-    digitalWrite(MOTOR3LED,LOW);
-    digitalWrite(MOTOR4LED,LOW);
-    delay(200);
-  }
-}*/
-
-/*
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
-RF24 radio(7, 8); // CE, CSN
-
-const byte address[6] = "00001";
-
-void setup() {
-  Serial.begin(115200);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
-  pinMode(2,OUTPUT);
-}
-
-void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-    digitalWrite(2,HIGH);
-  }else {
-    Serial.println("NOP");
-    digitalWrite(2,HIGH);
-    delay(100);
-    digitalWrite(2,LOW);
-    delay(100);
-  }
-  delay(200);
-}
-*/
-
 #include <Servo.h>
 Servo ESC1;
 Servo ESC2;
@@ -210,16 +37,18 @@ void calibration(){
   ESC3.writeMicroseconds(2000);
   ESC4.writeMicroseconds(2000);
 
-  delay(3000);
+  delay(2000);
 
   ESC1.writeMicroseconds(1000);
   ESC2.writeMicroseconds(1000);
   ESC3.writeMicroseconds(1000);
   ESC4.writeMicroseconds(1000);
 
-  delay(3000);
+  delay(2000);
   Serial.println("DONE CALIBRATION");
-  ledBlink();
+  for(int iter=0; iter<2; iter++){
+    ledBlink();
+  }
 }
 
 void motorTest(){
@@ -272,6 +101,7 @@ void setup() {
 
   calibration();
   //motorTest();
+  delay(3000);
 }
 
 void ledBlink(){
@@ -308,7 +138,7 @@ void emergencyLanding(){
     ESC2.writeMicroseconds(throtle - 100);
     ESC3.writeMicroseconds(throtle - 100);
     ESC4.writeMicroseconds(throtle - 100);
-    delay(100);
+    delay(1000);
 
     throtle -= 100;
   }
@@ -352,7 +182,8 @@ void loop() {
     int yaw = userIn.yaw;
     int pitch = userIn.pitch;
     int roll = userIn.roll;
-    int throtle = userIn.throttle;
+    int throtle = atoi(userIn.throttle);
+    throtle = constrain(throtle, 0, 100); 
     throtle = map(throtle,0,100,1000,2000);
 
     ESC1.writeMicroseconds(throtle);
@@ -400,6 +231,7 @@ void loop() {
     }
     digitalWrite(2,LOW);
   }else {
+    Serial.println("NOPE");
     digitalWrite(2,HIGH);
   }
 }
