6d3fbd0dff7c782f82ab351277f1da2b5ec23461
bohmid <elbo7ilahi@gmail.com>
2026-05-28 01:26:44 +0100

    fix: sequence of function calling

--- src/controller/controller/src/main.cpp
+++ src/controller/controller/src/main.cpp
@@ -48,8 +48,8 @@ void loop(void) {
   u8g2.firstPage();
   do {
     drawText();
-    drawValues(9999, 8888, 7777, 100, 555, userCollection.altitude);
     getUserJoysticks(JOYSTICK1X, JOYSTICK1Y, JOYSTICK2X, JOYSTICK2Y);
+    drawValues(9999, 8888, 7777, 100, 555, userCollection.altitude);
     userCollection.altitude = map(analogRead(POTENTIOMETER),0,4096,0,1024);
     setFLAGS(digitalRead(JOYSTICK1BTN),digitalRead(JOYSTICK2BTN));
     Serial.printf("ROLL: %d\tPITCH: %d\tYAW: %d\tTHROTTLE: %d\tALTITUTDE: %d\tBTN1: %d\tBTN2: %d\n",
